9cf8ad3de0917340ca218d7142fc7062839b30e2
Ali Chegini <ali.c.engineer@gmail.com>
2019-09-03 10:03:16 +0200

    initial commit - storyboard deleted

---
+++ RSR/MainViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  RSR
+//
+//  Created by Ehsan on 03/09/2019.
+//  Copyright © 2019 Ali C. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        navigationItem.title = "RSR"
+        view.backgroundColor = .green
+
+    }
+
+
+}
+
